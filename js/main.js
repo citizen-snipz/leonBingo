d9c93ca0ec7247400479b1e57033d374bce245da
Shara Crosslin <sharacrosslin@gmail.com>
2020-10-12 10:47:24 -0700

    Added JS buttons for read status and removing book, additional CSS styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let bingoSpots = ['Someone compliments Leon\'s hair', 'Someone types "organization" in the chat before a question is asked', 'Someone emotes a Micro Leon in chat', 'Leon says octothorpe', 'Leon says "automagically"', '"A variable is a bucket"', 'Leon forgets to turn off the background music', 'Someone finishes a challenge before the timer goes off', 'Someone mentions the OnlyFans page', 'Alissa posts a helpful link in chat', 'Leon tells us what he is drinking today', 'Everyone freaks out about operators', 'Hydrate!', 'Posture Check!']
+let bingoSpots = ['Someone compliments Leon\'s hair', 'Someone preemptively types "organization" in the chat', 'Someone emotes a Micro Leon in chat', 'Leon says octothorpe', 'Leon says "automagically"', '"A variable is a bucket"', 'Leon forgets to turn off the background music', 'Someone finishes a challenge before the timer goes off', 'Someone mentions the OnlyFans page', 'Alissa posts a helpful link in chat', 'Leon tells us what he is drinking today', 'Everyone freaks out about operators', 'Hydrate!', 'Posture Check!']
     let para = document.createElement('p');
 
     function randomizeBingoSpots() {
