582c032f1aacc28975b219b097a2a7b3f9bb2ec7
James Crosslin <jamesmscrosslin@gmail.com>
2020-11-05 15:02:32 -0800

    Phrase changes for current week
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- js/weeklyPhrases.js
+++ js/weeklyPhrases.js
@@ -1,19 +1,37 @@
 const weeklyPhrases = [
-  "The Golden Rule 👑👑👑",
+  "The Golden Rule: Seperation of Concerns 👑👑👑",
   "What is the internet? 💻 🖥️",
-  "angry Shia LaBeouf",
-  "Hello world!",
-  "Size doesn't matter, okay? OKAY???",
-  "CSS is about <em>style</em>",
-  "Ordered List",
-  "Unordered List",
+  "Hello twitch!",
   `Syntax:
    Spelling and Grammar rules`,
   'Someone preemptively types "organization" in the chat 👩🏽‍💻',
   "Resetting the Forgetting Curve 😱",
-  "What should go in the head tag?",
-  "anchor tags are for links ⚓",
-  "Progressive Enhancement"
+  "Progressive Enhancement!",
   //    "binary, upload, boom 👽",
   // "Everyone freaks out about operators",
+  "Repetition Repetition Repetition Repetition Repetition Repetition",
+  "CSS goes in the head 🗣️",
+  "Don't know? Go to the MDN!",
+  "Selectors are about relationships",
+  "Only ONE id with the same value per document!",
+  "Someone figures out the specificity before you even started"
 ]
+/*
+const weekOnePhrases = [
+    "The Golden Rule 👑👑👑",
+    "What is the internet? 💻 🖥️",
+    "angry Shia LaBeouf",
+    "Hello world!",
+    "Size doesn't matter, okay? OKAY???",
+    "CSS is about <em>style</em>",
+    "Ordered List",
+    "Unordered List",
+    `Syntax:
+     Spelling and Grammar rules`,
+    'Someone preemptively types "organization" in the chat 👩🏽‍💻',
+    "Resetting the Forgetting Curve 😱",
+    "What should go in the head tag?",
+    "anchor tags are for links ⚓",
+    "Progressive Enhancement"
+  ]
+*/
