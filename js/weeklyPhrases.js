ad8d6f414df427d839a1c34ba5be2f11d5816e27
James Crosslin <jamesmscrosslin@gmail.com>
2020-11-19 14:55:27 -0800

    Weekly update
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- js/weeklyPhrases.js
+++ js/weeklyPhrases.js
@@ -17,10 +17,11 @@ const weeklyPhrases = [
   `<a href="http://css-tricks.com">css-tricks.com</a>: your CSS bible`,
   "Tried to use a media query without viewport <meta> tag",
   "Make good content decisions",
-  "Today's stranger is tomorrow's coworker",
+  "Stranger to coworker pipeline",
   "Insert unpaid meetup.com advertisement",
-  "A lime alien that will haunt your dreams",
-  "Spaced follow-up is important"
+  "A lime alien that will life forever in your nightmares",
+  "Spaced follow-up is important",
+  "Progressive Enhancement"
 ]
 /*
 const weekOnePhrases = [
@@ -61,5 +62,32 @@ const weekTwoPhrases = [
   "Only ONE id with the same value per document!",
   "Someone figures out the specificity before you even started",
   "You messed up"
+]
+*/
+
+/*
+const week3Phrases = [
+  `The Golden Rule: Seperation of Concerns
+  👑👑👑`,
+  'Someone preemptively types "organization" in the chat 👩🏽‍💻',
+  "CSS goes in a separate file",
+  "Don't know? Go to the MDN!",
+  "Get your selector relationships in order",
+  "Only ONE id with the same value per document!",
+  "Someone figures out the specificity before you even start",
+  "Cascade - lower rules can override the ones above!",
+  "The Box Model:  Content > Padding > Border > Margin",
+  "We all float down here; you'll float too 🎈🎈🎈",
+  "Get your shit together UPS",
+  "Fluid: EVERYTHING IS A PERCENTAGE %%%",
+  "Ems and Rems keep your font sizes elastic",
+  "Flexbox is magic, y'all ✨✨✨",
+  `<a href="http://css-tricks.com">css-tricks.com</a>: your CSS bible`,
+  "Tried to use a media query without viewport <meta> tag",
+  "Make good content decisions",
+  "Today's stranger is tomorrow's coworker",
+  "Insert unpaid meetup.com advertisement",
+  "A lime alien that will haunt your dreams",
+  "Spaced follow-up is important"
 ]
 */
