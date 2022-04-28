1e9c3f0730c2377af8e2ee37a7f827b79cdd84f0
Shara Crosslin <sharacrosslin@gmail.com>
2022-04-28 15:28:50 -0700

    update for 4-28-22 stream

--- js/phraseBank.js
+++ js/phraseBank.js
@@ -6,7 +6,7 @@
 const phraseBank = [
   // "Leon says octothorpe",
   "Leon says 'automagically' \r✨✨✨✨",
-  '10 people finish a challenge before the timer goes off ⏲️',
+  //'10 people finish a challenge before the timer goes off ⏲️',
   'Leon mentions the OnlyFans page 🔞 ',
   // "Alissa posts a helpful link in chat 🔗",
   'Leon tells us what he is drinking today',
